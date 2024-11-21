1dfc73472ad643f470edebbdf9c28765666e1e3e
Laura Roque <lroquecu@gmail.com>
2024-11-21 10:24:58 +0100

    s3

---
+++ s3.tf
@@ -0,0 +1,23 @@
+# S3 Bucket
+resource "aws_s3_bucket" "mikrotik_bucket" {
+  bucket = var.bucket_name
+}
+
+# S3 trigger for Lambda
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.mikrotik_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.mikrotik_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = var.input_file_path
+    filter_suffix       = ".csv"
+  }
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.mikrotik_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = var.input_file_path
+    filter_suffix       = ".xlsx"
+  }
+}
