ff75dbf69838f835fabf83884bb0e98527c715a8
Laura Roque <lroquecu@gmail.com>
2024-11-21 10:12:35 +0100

    variables

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+
+}
+
+variable "input_file_path" {
+  description = "The path for the input files"
+  type        = string
+
+}
+
+variable "output_file_path" {
+  description = "The path for the output files"
+  type        = string
+
+}
