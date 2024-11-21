18a3d158a37af40d3dccea60ecbbe92a6e07cbf5
Laura Roque <lroquecu@gmail.com>
2024-11-21 10:13:33 +0100

    add project name

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+
+}
+
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
