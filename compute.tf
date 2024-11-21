86f48c98a5d4775ea187a067673fa5be696edb11
Laura Roque <lroquecu@gmail.com>
2024-11-21 11:03:18 +0100

    format

--- compute.tf
+++ compute.tf
@@ -50,13 +50,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 
 # Lambda function
 resource "aws_lambda_function" "mikrotik_processor" {
-  filename         = "lambda_function.zip"
-  function_name    = "mikrotik_processor"
-  role            = aws_iam_role.lambda_role.arn
-  handler         = "lambda_function.lambda_handler"
-  runtime         = "python3.9"
-  timeout         = 180
-  memory_size     = 256
+  filename      = "lambda_function.zip"
+  function_name = "mikrotik_processor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 180
+  memory_size   = 256
 
   environment {
     variables = {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-project_name = "script-for-mikrotik-netwatch"
-bucket_name = "${var.project_name}-bucket"
-input_file_path = "clientes/equipos/"
+project_name     = "script-for-mikrotik-netwatch"
+bucket_name      = "${var.project_name}-bucket"
+input_file_path  = "clientes/equipos/"
 output_file_path = "mikrotiks/equipos/"
